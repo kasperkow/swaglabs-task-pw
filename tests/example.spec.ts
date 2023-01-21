8ff3cbe3d9ca8ea55e8399b08eba8bba4d5b0a0e
Kasper <kkowalik@clickmeeting.com>
2023-01-21 20:58:10 +0100

    add login tests

--- tests/example.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect } from '@playwright/test'
-
-test('true', async ({ page }) => {
-    expect(true).toBeTruthy()
-})
-
-test('Visit base page', async ({ page }) => {
-    await page.goto(`/`)
-})
---
+++ tests/login.spec.ts
@@ -0,0 +1,22 @@
+import { test, expect } from '@playwright/test'
+test.describe('Login', () => {
+    test('User should login to page with valid credentials', async ({ page }) => {
+        await page.goto('')
+        await page.locator('[data-test="username"]').fill('standarduser')
+        await page.locator('[data-test="username"]').fill('standard_user')
+        await page.locator('[data-test="password"]').fill('secret_sauce')
+        await page.locator('[data-test="login-button"]').click()
+        await expect(page).toHaveURL('inventory.html')
+    })
+
+    test('User should not login when account is locked', async ({ page }) => {
+        await page.goto('')
+        await page.locator('[data-test  ="username"]').fill('standarduser')
+        await page.locator('[data-test="username"]').fill('locked_out_user')
+        await page.locator('[data-test="password"]').fill('secret_sauce')
+        await page.locator('[data-test="login-button"]').click()
+        const warning = page.locator('[data-test="error"]')
+        await expect(warning).toBeVisible()
+        await expect(page).toHaveURL('')
+    })
+})
