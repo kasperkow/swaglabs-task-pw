76cd87a33dbd396fc6fefba9020273c0efb9bac2
Kasper <kkowalik@clickmeeting.com>
2023-01-21 20:42:52 +0100

    playwright config update

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -3,3 +3,7 @@ import { test, expect } from '@playwright/test'
 test('true', async ({ page }) => {
     expect(true).toBeTruthy()
 })
+
+test('Visit base page', async ({ page }) => {
+    await page.goto(`/`)
+})
