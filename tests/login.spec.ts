1e99248791300b34374bd31657daacb9be720a1b
Kasper <kkowalik@clickmeeting.com>
2023-01-21 21:24:30 +0100

    add env file to store password

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -2,18 +2,16 @@ import { test, expect } from '@playwright/test'
 test.describe('Login', () => {
     test('User should login to page with valid credentials', async ({ page }) => {
         await page.goto('')
-        await page.locator('[data-test="username"]').fill('standarduser')
         await page.locator('[data-test="username"]').fill('standard_user')
-        await page.locator('[data-test="password"]').fill('secret_sauce')
+        await page.locator('[data-test="password"]').fill(process.env.USER_PASSWORD)
         await page.locator('[data-test="login-button"]').click()
         await expect(page).toHaveURL('inventory.html')
     })
 
     test('User should not login when account is locked', async ({ page }) => {
         await page.goto('')
-        await page.locator('[data-test  ="username"]').fill('standarduser')
         await page.locator('[data-test="username"]').fill('locked_out_user')
-        await page.locator('[data-test="password"]').fill('secret_sauce')
+        await page.locator('[data-test="password"]').fill(process.env.USER_PASSWORD)
         await page.locator('[data-test="login-button"]').click()
         const warning = page.locator('[data-test="error"]')
         await expect(warning).toBeVisible()
